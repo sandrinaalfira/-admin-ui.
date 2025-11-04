8b91624716113e9e6f85cf53d28caedde20a24b7
sandrinaalfira <sandrinaalfira@gmail.com>
2025-11-04 11:33:55 +0700

    UTS Pemograman Sisi Klien

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
               <input
                 type="password"
                 className="py-3 pl-4 text-sm rounded-md w-full bg-special-mainBg border border-gray-03 text-gray-01 focus:border-black focus:outline-none focus:ring-0"
-                placeholder="************"
+                placeholder=""
                 name="password"
                 id="password"
               />
